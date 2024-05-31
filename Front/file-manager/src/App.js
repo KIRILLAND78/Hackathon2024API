8372b99972700a546710c0a9cd58af03b8afe152
phanilevanilehyile <56351259+Phanile@users.noreply.github.com>
2024-05-31 23:51:20 +0300

    Сделал при загрузке страницы подтягивание аксес токена

--- Front/file-manager/src/App.js
+++ Front/file-manager/src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Routes, Navigate, useNavigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
 import Login from './components/Login';
 import Register from './components/Register';
 import FileList from './components/FileList';
@@ -9,6 +10,30 @@ function App() {
   const [user, setUser] = useState(null);
   const [isAdmin, setIsAdmin] = useState(false);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      try {
+        const decodedToken = jwtDecode(accessToken);
+        const userData = {
+          email: decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"],
+          isAdmin: decodedToken["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"] === 'Admin'
+        };
+        setUser(userData);
+        setIsAdmin(userData.isAdmin);
+        if (userData.isAdmin) {
+          navigate('/admin');
+        } else {
+          navigate('/files');
+        }
+      } catch (error) {
+        console.error('Invalid token:', error);
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('refreshToken');
+      }
+    }
+  }, [navigate]);
 
   const handleLogin = (userData) => {
     setUser(userData);
