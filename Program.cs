06f8685fed005823a24105b5fed4593cdbedf0dd
KIRILLAND <55920722+KIRILLAND78@users.noreply.github.com>
2024-05-31 17:27:24 +0300

    шифрование файлов

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Hackathon2024API.Data;
+using Hackathon2024API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<EncryptionService>();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
